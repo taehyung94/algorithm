8f8537f21b1f934423b1282311fd185401f886a8
보보 <rlaxogud94@naver.com>
2020-02-19 18:34:54 +0900

    Update 26.cpp

--- source/26.cpp
+++ source/26.cpp
@@ -15,15 +15,12 @@ int main()
 	for (int i = 1; i <= n; i++)
 	{
 		int cnt = 0;
-		for (int j = i; j >= 1; j--)
-		{
-			if (arr[i] <= arr[j])
+		for (int j = i-1; j >= 1; j--)  // 자신보다 실력이 큰 사람의 숫자를 센다. 하지만 순서는 1등부터 시작하므로 +1 해서 출력!					    
+		{                             				
+			if (arr[i] <= arr[j]) // 
 				cnt++;
 		}
-		if(cnt==0)
-			cout << cnt+1 << ' ';
-		else
-			cout << cnt << ' ';
+		cout << cnt+1 << ' ';
 	}
 	cout << '\n';
 	return 0;
